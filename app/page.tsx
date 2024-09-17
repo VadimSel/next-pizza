f820f35ed566016367b925aed7a642fe7a851bba
VadimSel <selyaaa33@gmail.com>
2024-09-17 17:51:42 +0300

    add top bar layout, product filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+import { Container } from "@/components/shared/container";
+import { Filters } from "@/components/shared/filters";
+import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/*Фильтрация*/}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/*Список товаров*/}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
